58f7b46e830092219a3f1ff650f3b22b66cc92cf
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2024-09-10 22:30:11 +0700

    [Fix] remove unused path name import

--- components/Nav/MobileNav.tsx
+++ components/Nav/MobileNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import { CiMenuFries } from "react-icons/ci";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import Link from "next/link";
